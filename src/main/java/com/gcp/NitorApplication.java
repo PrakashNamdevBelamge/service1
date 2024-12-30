ed550668979f85803c37145b30d602b9db9306af
Prakash Namdev <prakash.namdev@nitorinfotech.com>
2024-12-30 15:18:08 +0530

    first-commit

---
+++ src/main/java/com/gcp/NitorApplication.java
@@ -0,0 +1,21 @@
+package com.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class NitorApplication {
+
+	@GetMapping
+	public String getMessage(){
+		return "Hello iam service1";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitorApplication.class, args);
+	}
+
+}
